360ded0529a28da7abf170beb72f3f058e01dc58
Remco <remcocoppens@outlook.com>
2022-10-20 16:52:05 +0200

    Managed dependencies

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(
+    name="Datacademy Bootcamp",
+    version="1.0",
+    packages=find_packages(),
+    author="Datacation",
+    author_email="r.coppens@datacation.nl",
+    description="Datacademy Bootcamp, brought to you by Datacation",
+    license="Datacation",
+    url="https://www.datacation.nl/"
+)
